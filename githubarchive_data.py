60d73dea83453b15981ecb5ef16d9226720b6b7c
mackenza <a.mackenzie@lancaster.ac.uk>
2013-05-26 22:03:57 +0100

    used pylint to clean up code

--- githubarchive_data.py
+++ githubarchive_data.py
@@ -1,19 +1,40 @@
+"""
+Functions that retrieve stuff from githubarchive.org
+and package it  in more usable forms
+
+To download and open directly from the githubarchive:
+
+- gets all the data for april:
+#wget http://data.githubarchive.org/2012-04-{01..31}-{0..23}.json.gz  
+
+- get 3-4 pm on the day:
+#wget http://data.githubarchive.org/2012-04-11-15.json.gz 
+
+- get he whole day:
+#wget http://data.githubarchive.org/2012-04-11-{0..23}.json.gz
+
+n.b. the earliest URL I could find that works: 
+#http://data.githubarchive.org/2011-02-12-15.json.gz
+
+"""
+
+
 import gzip
 import json
 import re
 import pandas as pn
-import os
 import requests
 import StringIO
 import numpy as np
 
 # chunks of code to fetch, unzip, load and get some fields from gitarchive.org
 
-#TODO:
+# @TODO:
 # 1. code to store json objects in a no-SQL database. Mongo perhaps easiest
 # 2. code to put useful extracts from data into SQL tables
 # 3. code to generate days for a given month
-# 4. calculate how much storage we'd need for all the githubarchive data (roughly 2 years worth)
+# 4. calculate how much storage we'd need for all the githubarchive data 
+# (roughly 2 years worth)
 
 
 #shameless copy paste from json/decoder.py
@@ -21,63 +42,68 @@ FLAGS = re.VERBOSE | re.MULTILINE | re.DOTALL
 WHITESPACE = re.compile(r'[ \t\n\r]*', FLAGS)
 
 class ConcatJSONDecoder(json.JSONDecoder):
-    def decode(self, s, _w=WHITESPACE.match):
-        s_len = len(s)
+
+    """Helper class for decoding compressed unicodedata"""
+
+    def decode(self, stri, whspace_match=WHITESPACE.match):
+        s_len = len(stri)
 
         objs = []
         end = 0
         while end != s_len:
-            obj, end = self.raw_decode(s, idx=_w(s, end).end())
-            end = _w(s, end).end()
+            obj, end = self.raw_decode(stri, idx=whspace_match(stri, end).end())
+            end = whspace_match(stri, end).end()
             objs.append(obj)
         return objs
 
-sample_file = 'data/2012-04-01-12.json.gz'
 
 
-def load_local_gz(sample_file):
-    """returns a DataFrame with all the data from one githubarchive gzip file."""
+def load_local_gz(sample_file  = 'data/2012-04-01-12.json.gz'):
 
-    gz = gzip.open(sample_file)
-    fc = gz.read()
-    gh= json.loads(fc, cls=ConcatJSONDecoder)
+    """returns a DataFrame with all the data 
+    from one githubarchive gzip file."""
 
-    # this creates a wide dataframe, with some fields nested. See below for a way of splitting them out. 
-    df_all = pn.DataFrame.from_dict(gh)
+    gzfile = gzip.open(sample_file)
+    decompressed_file = gzfile.read()
+    git_json = json.loads(decompressed_file, cls=ConcatJSONDecoder)
+
+    # this creates a wide dataframe, with some fields nested. 
+    # See below for a way of splitting them out. 
+    df_all = pn.DataFrame.from_dict(git_json)
     return df_all
 
-## to download and open directly from the githubarchive
-#wget http://data.githubarchive.org/2012-04-{01..31}-{0..23}.json.gz  # gets all the data for april
-#wget http://data.githubarchive.org/2012-04-11-15.json.gz # 3pm on the day
-#wget http://data.githubarchive.org/2012-04-11-{0..23}.json.gz # the whole day
-#the earliest URL I could find that works: http://data.githubarchive.org/2011-02-12-15.json.gz
 
-#example of getting an hours data from the archive
 
 def load_archive_gz_demo():
-    url = 'http://data.githubarchive.org/2012-04-1-15.json.gz' ##1-5 April 2012, 3-4pm
+
+    """Returns DataFrame with 1 hour of sample github data 
+    stored locally: 5 April 2012, 3-4pm"""
+
+
+    url = 'http://data.githubarchive.org/2012-04-1-15.json.gz' 
     response = requests.get(url)
-    compressedFile = StringIO.StringIO(response.content)
-    decompressedFile = gzip.GzipFile(fileobj=compressedFile)
-    fc = decompressedFile.read()
-    gh= json.loads(fc, cls=ConcatJSONDecoder)
-    df_all = pn.DataFrame.from_dict(gh)
+    compressed_file = StringIO.StringIO(response.content)
+    decompressed_file = gzip.GzipFile(fileobj=compressed_file)
+    decompressed_file = decompressed_file.read()
+    git_json = json.loads(decompressed_file, cls=ConcatJSONDecoder)
+    df_all = pn.DataFrame.from_dict(git_json)
     return df_all
 
 def construct_githubarchive_url(year=2012,  month=1, day=1,  hour = 12 ):
 
-    """ Returns a url formatted for date-time retrieval of one hour of githubarchive in a json files. 
+    """ Returns a url formatted for date-time retrieval of 
+    1 hour of githubarchive in a json file. 
     """
 
     base_url = 'http://data.githubarchive.org/'
     suffix = '.json.gz'
     
-    if month <10:
+    if month < 10:
         month = '0' + str(month)
     else:
         month = str(month)
     
-    if day <10:
+    if day < 10:
         day = '0' + str(day)
     else:
         day = str(day)
@@ -95,13 +121,13 @@ def fetch_archive(url):
 
     try:
         response = requests.get(url)
-        compressedFile = StringIO.StringIO(response.content)
-        decompressedFile = gzip.GzipFile(fileobj=compressedFile)
-        fc = decompressedFile.read()
-        gh= json.loads(fc, cls=ConcatJSONDecoder)
-        df_all = pn.DataFrame.from_dict(gh)
-    except Exception, e:
-        print e
+        compressed_file = StringIO.StringIO(response.content)
+        decompressed_file = gzip.GzipFile(fileobj=compressed_file)
+        decompressed_file = decompressed_file.read()
+        git_json = json.loads(decompressed_file, cls=ConcatJSONDecoder)
+        df_all = pn.DataFrame.from_dict(git_json)
+    except Exception, exce:
+        print exce
     return df_all
 
 
@@ -109,77 +135,85 @@ def fetch_one_day_data(year=2012, month=1, day=1):
 
     """ Returns a dataframe with 1 days github events."""
 
-    hours = range(0,24)
-    df = pn.DataFrame()
-    for h in hours:
-        url = construct_githubarchive_url(year, month, day, h)
-        df = df.append(fetch_archive(url))
+    hours = range(0, 24)
+    day_df   = pn.DataFrame()
+    for hour in hours:
+        url = construct_githubarchive_url(year, month, day, hour)
+        day_df   = day_df .append(fetch_archive(url))
         print 'fetching ' + url
-    return df
+    return day_df   
 
 
 def unnest_git_json(df_all):
     
-    """ Returns  a dictionary of the data nested in each json object. There are 3 nested dictionaries in the json object - repos, actor_attributes and payload.    """
+    """ Returns  a dictionary of the data nested in each json object. 
+    There are 3 nested dictionaries in the json object:
+     - repos, actor_attributes and payload.    """
     #for repository
     df_rep = pn.DataFrame.from_dict(df_all['repository'].to_dict()).transpose()
     #for users
-    df_actors  = pn.DataFrame.from_dict(df_all['actor_attributes'].to_dict()).transpose()
+    df_actors  = pn.DataFrame.from_dict(df_all['actor_attributes'].
+        to_dict()).transpose()
     #for payload -- whatever that is
-    df_payload  = pn.DataFrame.from_dict(df_all['payload'].to_dict()).transpose()
-
-    return {'all': df_all, 'repository':df_rep, 'actors': df_actors, 'payload':df_payload}
+    df_payload  = pn.DataFrame.from_dict(
+        df_all['payload'].to_dict()).transpose()
+    df_dict = {'all': df_all, 'repository':df_rep, 
+        'actors': df_actors, 
+        'payload':df_payload}
+    return  df_dict
 
 def github_event_explore(df_all):
 
     """ Returns a DataFrame with event types, and timestamps."""
 
-    dic ={'created_at': df_all['created_at'], 'type' :df_all['type']}
-    df = pn.Series(data = df_all['type'],index = df_all['created_at'])
+    event_df = pn.Series(data = df_all['type'], index = df_all['created_at'])
+    return event_df
 
-    df_eve.type.value_counts().plot(kind='bar')
-    return df
-
-def calculate_storage_needs_in_Gbytes(days=1):
+def calculate_storage_in_gbytes(days=1):
 
     """Returns a dictionary with compressed and uncompressed total gigabytes 
-    for githubarchive data by the day. The average hourly figure  of 1133000 compressed bytes
-    is based on an average filesize calculated on 700 files.
-    @TODO: calculate the uncompressed values based on the uncompressed size of 700 files
-    @TODO: the same calculations for stackoverflow -- Richard?
+        for githubarchive data by the day. The average hourly figure  of 1133000 compressed bytes
+        is based on an average filesize calculated on 700 files.
+        @TODO: calculate the uncompressed values based on the uncompressed size of 700 files
+        @TODO: the same calculations for stackoverflow -- Richard?
     """
 
     average_gz_file = float(1133000)
-    gb = 1024*1024*1024; #hope this is right
+    gbyte = 1024*1024*1024 #hope this is right
     #sample file only -- not necessarily representative!
-    f = gzip.GzipFile('data/2012-04-01-12.json.gz')
-    uc = float(len(f.read())) * days * 24/gb
+    gzip_file = gzip.GzipFile('data/2012-04-01-12.json.gz')
+    uncompressed_size = float(len(gzip_file.read())) * days * 24/gbyte
     #hour = os.path.getsize('data/2012-04-01-12.json.gz')
-    total_comp = days * average_gz_file *24/gb
-    return {'compressed': total_comp, 'uncompressed': uc}
+    total_comp = days * average_gz_file *24/gbyte
+    return {'compressed': total_comp, 'uncompressed': uncompressed_size}
 
-def calculate_average_uncompressed_hourly_data_size(hours=1):
+
+def uncompressed_hourly_average(hours=1):
     
-    """Returns an average hourly uncompressed data size based on a random sample of hours
+    """Returns an average hourly uncompressed data size 
+        based on a random sample of hours
         from 10 days over 3 years
         @TODO: this is broken -- needs debug
     """
+
     hrs = np.random.random_integers(0, 24, hours)
     days = np.random.random_integers(1, 28, 10)
     months = np.random.random_integers(1, 12, 3)
     years = np.random.random_integers(2011, 2013, 3)
     files = []
-    for y in years:
-        for m in months:
-            for d in days:
-                for h in hrs:
-                    files.append(construct_githubarchive_url(y, m, d, h))
-     for f in files:
+    for year in years:
+        for month in months:
+            for day in days:
+                for hour in hrs:
+                    files.append(
+                        construct_githubarchive_url(year, month, day, hour))
+    for fil in files:
         try:
-            response = requests.get(f)
-            compressedFile = StringIO.StringIO(response.content)
-            uc = float(len(gzip.GzipFile(fileobj=compressedFile).read()))
-            print uc
-        except Exception, e:
-            print e
-     
+            response = requests.get(fil)
+            compressed_file = StringIO.StringIO(response.content)
+            uncompressed_size = float(len(gzip.GzipFile(
+                fileobj=compressed_file).read()))
+            print uncompressed_size
+        except Exception, exce:
+            print exce
+
